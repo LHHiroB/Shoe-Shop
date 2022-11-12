f7466e4d2e560a6d2ac06344cbee9d0914c8183d
abc <khoahieu2982002@gmail.com>
2022-11-12 23:22:22 +0700

    alo alo

--- src/js/main.js
+++ src/js/main.js
@@ -5,3 +5,17 @@ menu.onclick =() =>{
     menu.classList.toggle('fa-times');
     navbar.classList.toggle('active');
 }
+let slides = document.querySelectorAll('.slide-container');
+let index = 0;
+
+function next(){
+    slides[index].classList.remove('active');
+    index = (index + 1) % slides.length;
+    slides[index].classList.add('active');
+}
+
+function prev(){
+    slides[index].classList.remove('active');
+    index = (index - 1 + slides.length) % slides.length;
+    slides[index].classList.add('active');
+}
