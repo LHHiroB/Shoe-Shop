a40e0cfa9f56225d8b38324f9a9bb1adac6cbdfc
Huệ Nguyễn Tấn <20521359@ms.uit.edu.vn>
2022-11-18 18:18:22 +0700

    add files

--- src/js/main.js
+++ src/js/main.js
@@ -1,21 +1,32 @@
+
 let menu = document.querySelector('#menu-bar');
 let navbar = document.querySelector('.navbar');
 
-menu.onclick =() =>{
-    menu.classList.toggle('fa-times');
+menu.onclick = () => {
+    // menu.classList.toggle('fa-times');
     navbar.classList.toggle('active');
-}
-let slides = document.querySelectorAll('.slide-container');
-let index = 0;
-
-function next(){
-    slides[index].classList.remove('active');
-    index = (index + 1) % slides.length;
-    slides[index].classList.add('active');
+       
 }
 
-function prev(){
-    slides[index].classList.remove('active');
-    index = (index - 1 + slides.length) % slides.length;
-    slides[index].classList.add('active');
+// scroll then remove header 
+let section = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header .navbar a');
+
+window.onscroll = () => {
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+    section.forEach(sec => {
+        let top = window.scrollY;
+        let height = sec.offsetHeight;
+        let offset = sec.offsetTop - 150;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height){
+            navLinks.forEach(links =>{
+                links.classList.remove('active');
+                document.querySelector('header .navbar a[href*='+id+']').classList.add('active');
+            })
+        }
+    })
 }
+
